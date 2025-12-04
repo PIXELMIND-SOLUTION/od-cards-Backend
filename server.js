0d7dbeb7f36506edc3fe7ddeade813ed6748b218
ganapathi9191 <ganapathivaraprasad123@gmail.com>
2025-12-04 19:10:51 +0530

    04-12-2025

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.use('/api/contactus', Contactus);
 app.use("/api/banners", bannerRoutes);
 app.use('/api/marquees', marqueeRoutes);
 
-const PORT = process.env.PORT || 5124;
+const PORT = process.env.PORT || 9124;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
